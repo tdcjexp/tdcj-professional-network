d029621761fbe04297154074e95e8e23bd2b8c38
tdcjexp <tdcjexp@gmail.com>
2026-05-23 19:33:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 function comingSoon(featureName) {
     alert(featureName + " will be added in a later version.");
 }
+
+function sendMessage(event) {
+    event.preventDefault();
+
+    let recipient = document.getElementById("recipient").value;
+    let message = document.getElementById("messageText").value;
+
+    if(recipient === "" || message === "") {
+        alert("Please complete all fields.");
+        return;
+    }
+
+    alert("Message sent to " + recipient);
+
+    document.getElementById("recipient").value = "";
+    document.getElementById("messageText").value = "";
+}
